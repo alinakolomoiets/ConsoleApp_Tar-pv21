94b1d243023425c7627c5e7873f45405209e6819
Alina <93324365+alinakolomoiets@users.noreply.github.com>
2022-02-09 00:42:10 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,35 @@ namespace ConsoleApp_Tar_pv21
 	{
 		static void Main(string[] args)
 		{
+			 //}
+                //int plosad = 0;
+                //Console.WriteLine("Длина первой стены ");
+                //int dlina1 = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Длина второй стены ");
+                //int dlina2 = int.Parse(Console.ReadLine());
+                //plosad = dlina1 * dlina2;
+                //Console.WriteLine($"Площадь пола = {plosad}");
+                //Console.WriteLine("Ремонт запилить?");
+                //string remont = Console.ReadLine();
+                /* if (remont.ToUpper() == "YES" || remont.ToUpper() == "ДА")
+              {
+                  Console.WriteLine("Сколько стоит метр?");
+                  int mt = int.Parse(Console.ReadLine());
+                  summa = plosad * mt ;
+                  Console.WriteLine($"Вы заплатите {summa}");
+              }
+              else
+              {
+                  Console.WriteLine("досвидули!");
+              }
+              /*Console.ReadKey();
+                //Console.WriteLine("Сколько градусов в вашей комнате?");
+                //int answer = int.Parse(Console.ReadLine());
+                //if (answer >= 18)
+                //{
+                //    Console.WriteLine("Ваша температура в комнате выше 18 градусов");
+                //    string otvet = Console.ReadLine();
+                //}
 			
 			//Console.WriteLine("Tere nimeta nime pinginaabri => ");
 			//string n1 = Console.ReadLine();
